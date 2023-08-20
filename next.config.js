0868b727dfe0b10a1a0dfcf4bb6ff8fca31f4b8f
preetam-sharma <preetamsharma915@gmail.com>
2023-08-20 22:06:47 +0530

    image path source added to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "tymxmcjjwwnpoqbvapoq.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
